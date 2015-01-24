0e9e6a4b88811a2e02585b632d3dde0a5ee18728
hqtay <tayhuiqithq@gmail.com>
2015-01-23 20:56:58 -0800

    base scene

---
+++ WeXI/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public float speed = 12;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Moves player left, right, up, down. No collision.
+		float x = Input.GetAxis("Horizontal") * Time.smoothDeltaTime * speed;
+		float y = Input.GetAxis("Vertical") * Time.smoothDeltaTime * speed;
+		transform.Translate(x,y,0,Space.Self);
+	}
+}
