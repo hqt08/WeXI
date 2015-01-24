74fc9e5398ad287cfbe64742e8b264cca3ca63f5
hqtay <tayhuiqithq@gmail.com>
2015-01-24 14:49:35 -0800

    More I animations

---
+++ WeXI/Assets/Scripts/IEnemyScriptC2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IEnemyScriptC2 : MonoBehaviour {
+
+	public float height;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void triggerEnemy()
+	{
+		iTween.ScaleBy(gameObject, iTween.Hash("x", 4, "y", 5, "time", 0.7f));
+		iTween.MoveTo(gameObject,iTween.Hash("y",transform.position.y + height, "time", 0.7f, "easetype",iTween.EaseType.linear,"oncomplete","starthammering"));
+	}
+
+	public void starthammering()
+	{
+		Debug.Log("hammer");
+		iTween.RotateBy(gameObject, iTween.Hash("z", 0.3f, "time", 0.3f, "easetype", iTween.EaseType.easeInQuad, "looptype", iTween.LoopType.pingPong));
+	}
+
+	void destroyTheObject()
+	{
+		Destroy(gameObject);
+	}
+}
