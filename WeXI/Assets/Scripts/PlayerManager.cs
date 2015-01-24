83ad0698dde7346e7a26bdba86e86517f246ebf2
smallya <swatimallya@hotmail.com>
2015-01-24 14:01:28 -0800

    Created Level 2
    Currently working on adding more "i"s

--- WeXI/Assets/Scripts/PlayerManager.cs
+++ WeXI/Assets/Scripts/PlayerManager.cs
@@ -26,7 +26,7 @@ public class PlayerManager : MonoBehaviour {
 		if(isTest == false)
 		{
 			//Starting animation of the player which moves the player at start position
-			iTween.MoveTo(gameObject,iTween.Hash("delay",0.5f,"x",transform.position.x - 3.0f,"y",transform.position.y + 3.0f,"time",2.0f,"easetype",iTween.EaseType.linear,"oncomplete","giveControl"));
+			iTween.MoveTo(gameObject,iTween.Hash("delay",0.5f,"x",transform.position.x - 2.0f,"y",transform.position.y + 3.0f,"time",1.0f,"easetype",iTween.EaseType.linear,"oncomplete","giveControl"));
 		}
 		else
 		{
