b6d5a7f053568d327a22421ec0ab48070cabc663
smallya <swatimallya@hotmail.com>
2015-01-25 14:35:06 -0800

    Abstract Scene
    Update build settings

---
+++ WeXI/Assets/Scripts/AbstractButtonScript.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractButtonScript : MonoBehaviour {
+
+	public string name;
+
+	//animation
+	public GameObject we;
+	public GameObject I;
+
+	//other buttons
+	public GameObject back;
+	public GameObject next;
+	public GameObject menu;
+	public GameObject replay;
+
+	// Use this for initialization
+	void Start () {
+		back.GetComponent<SpriteRenderer>().renderer.enabled = false;
+		replay.GetComponent<SpriteRenderer>().renderer.enabled = false;
+		menu.GetComponent<SpriteRenderer>().renderer.enabled = true;
+		next.GetComponent<SpriteRenderer>().renderer.enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(we.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Abstract_We_Idle") &&
+		   I.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("I_Normal") &&
+		   name=="Replay")
+		{
+			this.enabled = true;
+		}
+		else
+		{
+			this.enabled = false;
+		}
+	
+	}
+
+	void OnMouseUp()
+	{
+		if(name=="Replay")
+		{
+			Debug.Log("Replay");
+			we.GetComponent<Animator>().CrossFade("Abstract_We_Move",0f);
+			I.GetComponent<Animator>().CrossFade("I_Awake",0f);
+		}
+		else if(name=="Back")
+		{
+			GameObject.Find("AbstractController").GetComponent<AbstractController>().SendMessage("page1");
+			this.GetComponent<SpriteRenderer>().renderer.enabled = false;
+			this.collider2D.enabled = false;
+			replay.GetComponent<SpriteRenderer>().renderer.enabled = false;
+			replay.collider2D.enabled = false;
+			menu.GetComponent<SpriteRenderer>().renderer.enabled = true;
+			menu.collider2D.enabled = true;
+			next.GetComponent<SpriteRenderer>().renderer.enabled = true;
+			next.collider2D.enabled = true;
+		}
+		else if(name=="Next")
+		{
+			GameObject.Find("AbstractController").GetComponent<AbstractController>().SendMessage("page2");
+			this.GetComponent<SpriteRenderer>().renderer.enabled = false;
+			this.collider2D.enabled = false;
+			replay.GetComponent<SpriteRenderer>().renderer.enabled = true;
+			replay.collider2D.enabled = true;
+			menu.GetComponent<SpriteRenderer>().renderer.enabled = false;
+			menu.collider2D.enabled = false;
+			back.GetComponent<SpriteRenderer>().renderer.enabled = true;
+			back.collider2D.enabled = true;
+		}
+		else if(name=="Menu")
+		{
+			Application.LoadLevel("Menu");
+		}
+	}
+}
---
+++ WeXI/Assets/Scripts/AbstractController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractController : MonoBehaviour {
+
+	public GameObject abstract1;
+	public GameObject abstract2;
+	public GameObject we2;
+	public GameObject I2;
+
+	// Use this for initialization
+	void Start () {
+		abstract2.GetComponent<SpriteRenderer>().renderer.enabled = false;
+		we2.GetComponent<SpriteRenderer>().renderer.enabled = false;
+		I2.GetComponent<SpriteRenderer>().renderer.enabled = false;
+
+		abstract1.GetComponent<SpriteRenderer>().renderer.enabled = true;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void page1()
+	{
+		abstract2.GetComponent<SpriteRenderer>().renderer.enabled = false;
+		we2.GetComponent<SpriteRenderer>().renderer.enabled = false;
+		I2.GetComponent<SpriteRenderer>().renderer.enabled = false;
+
+		abstract1.GetComponent<SpriteRenderer>().renderer.enabled = true;
+	}
+
+	void page2()
+	{
+		abstract2.GetComponent<SpriteRenderer>().renderer.enabled = true;
+		we2.GetComponent<SpriteRenderer>().renderer.enabled = true;
+		I2.GetComponent<SpriteRenderer>().renderer.enabled = true;
+
+		abstract1.GetComponent<SpriteRenderer>().renderer.enabled = false;
+	}
+
+}
