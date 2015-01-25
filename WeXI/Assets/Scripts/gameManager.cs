9fe84fdc0f4ce2f6ae33517d6f7efd64173a79c8
yxm0621 <yxm0621@gmail.com>
2015-01-24 23:21:04 -0800

    modify gameManager

--- WeXI/Assets/Scripts/gameManager.cs
+++ WeXI/Assets/Scripts/gameManager.cs
@@ -8,12 +8,16 @@ public class gameManager : MonoBehaviour {
 	public Text scoreText;
 	private AudioManager audio;
 	private bool hasPlayed;
+	private const int STATE_GAME = 0;
+	private const int STATE_DIE = 1;
+	private const int STATE_WIN = 2;
+	private const int STATE_MENU = 3;
 	
 	// Use this for initialization
 	void Start () {
-		gameState = 0;
+		gameState = STATE_GAME;
 		score = 0;
-		scoreText.text = "SCORE : "+score;
+
 		audio = GameObject.Find("Game Manager").GetComponent<AudioManager>();
 		hasPlayed = false;
 		
@@ -37,7 +41,26 @@ public class gameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		switch (gameState) { 
+		case STATE_GAME:
+			renderScore();
+			break;
+		case STATE_DIE:
+			
+			break;
+		case STATE_WIN:
+			
+			break;
+		case STATE_MENU:
+			
+			break;
+			
+		}
 		
+	}
+	
+	void renderScore(){
+		scoreText.text = "SCORE : "+score;
 	}
 
 	public void win() {
@@ -46,6 +69,7 @@ public class gameManager : MonoBehaviour {
 			audio.win.Play ();
 			hasPlayed = true;
 		}
+		gameState = STATE_WIN;
 		GameObject.Find("PageFlipper").animation.Play("page-flip");
 		StartCoroutine("LoadNextLevel");
 	}
